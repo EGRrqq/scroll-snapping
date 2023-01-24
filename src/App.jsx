3fed25f39faa28a4a25fc51fc4617db6f02717a1
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-01-24 19:18:21 +0300

    vertical scroll-snapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="snap-y snap-mandatory h-screen w-screen overflow-x-hidden">
+          <div className="snap-start bg-fuchsia-200 h-screen w-screen flex items-center justify-center text-5xl">1</div>
+          <div className="snap-start bg-cyan-200 h-screen w-screen flex items-center justify-center text-5xl">2</div>
+          <div className="snap-start bg-yellow-200 h-screen w-screen flex items-center justify-center text-5xl">3</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
   )
 }
 
